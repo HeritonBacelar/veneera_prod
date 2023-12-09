b26101229eac2dcd9b879413a36d92a807239514
HeritonBacelar <baacelar@gmail.com>
2023-12-09 18:55:22 +0100

    prod_create finished

--- prod_create.php
+++ prod_create.php
@@ -1,4 +1,29 @@
-<?php   
+<?php  
+if (isset($_POST['submit'])) { 
+    
+    include_once('dbConnection.php'); 
+    
+    $order_number = $_POST['order_number']; 
+    $veneer_set = $_POST['veneer_set'];   
+    $top_set = $_POST ['top_set'];  
+    $bottom_set = $_POST['bottom_set'];   
+    $IBI_reason = $_POST['IBI_reason']; 
+    $impression_status = $_POST['impression_status']; 
+    $info=$_POST['info'];  
+    
+    $tableName = "default_table";
+
+    if($impression_status === 'good'){  
+        $tableName = 'suitble_impressions';
+
+    } else { 
+        $tableName = 'notsuitble_impressions';
+    } 
+
+    $result = mysqli_query($conn, "INSERT INTO $tableName (order_number, veneer_set, top_set, bottom_set, IBI_reason, impression_status, info)  
+    VALUES ('$order_number', '$veneer_set', '$top_set', '$bottom_set', '$IBI_reason', '$impression_status', '$info')");
+
+}  
 
 ?>  
 
@@ -12,28 +37,28 @@
                 <input type="text" id="order_number" name="order_number" required> 
 
                 <label for="veneer_set">Veneer set:</label>
-                <select id="aveneer_set" name="veneer_set" required>
+                <select id="veneer_set" name="veneer_set" required>
                     <option value="">--Select--</option>
                     <option value="top">Top</option> 
                     <option value="bottom">Bottom</option> 
                     <option value="top/bottom">Top and Bottom</option>
                 </select>
 
-                <label for="top">Top Inspec:</label>
-                <select id="top" name="top" required>
+                <label for="top_set">Top Inspec:</label>
+                <select id="top_set" name="top_set" required>
                 <option value="">--Select--</option>
                     <option value="IBI">IBI</option> 
                     <option value="IBS">IBS</option> 
-                    <option value="doesn't apply">doesn't apply</option> 
+                    <option value="unused">unused</option> 
                     
                 </select> 
                 
-                <label for="bottom">Bottom Inspec:</label>
-                <select id="bottom" name="bottom" required>
+                <label for="bottom_set">Bottom Inspec:</label>
+                <select id="bottom_set" name="bottom_set" required>
                 <option value="">--Select--</option>
                     <option value="IBI">IBI</option> 
                     <option value="IBS">IBS</option> 
-                    <option value="doesn't apply">doesn't apply</option> 
+                    <option value="unused">unused</option> 
                     
                 </select> 
                 
@@ -43,16 +68,18 @@
                 <option value="">--Select--</option>
                     <option value="CW">CW</option> 
                     <option value="NET">NET</option> 
-                    <option value="doesn't apply">doesn't apply</option> 
+                    <option value="TG">TG</option>  
+                    <option value="unused">unused</option>  
+                    <option value="n/a">n/a</option>
                     
                 </select> 
                 
                 <label for="impression_status">Impression status</label>
                 <select id="impression_status" name="impression_status" required>
                 <option value="">--Select--</option>
-                    <option value="Completely wrong ">Completely wrong </option> 
+                    <option value="Completely wrong ">completely_wrong </option> 
                     <option value="good">good</option> 
-                    <option value="partially good">partially good</option> 
+                    <option value="partially good">partially_good</option> 
                     <option value="unused">unused</option> 
                     
                 </select> 
