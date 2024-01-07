e6ed7cf997cc8f702f25fd2075f0e0835ce89ffa
HeritonBacelar <baacelar@gmail.com>
2024-01-07 22:49:30 +0100

    refactoring

--- README.md
+++
@@ -1,3 +0,0 @@
-# veneera_workflow  
-
-
--- rcl_is_update.php
+++
@@ -1,9 +0,0 @@
-<?php   
-
-include_once ('dbConnection.php');   
-
-$id = $_GET['rcl_is_id'];
-
-$result = mysqli_query ($conn, "SELECT * FROM rcl_impression_set WHERE rcl_is_id = $id"); 
-
-?>
